146ea6a92e13c315bc7bb75c2d8ead806a23591f
malikelarbi <malik.elarbi@kellogg.northwestern.edu>
2022-03-16 04:53:37 +0000

    k

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,7 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+    @places = Place.where({ user_id: @current_user.id })
   end
 
   def show
@@ -14,6 +14,7 @@ class PlacesController < ApplicationController
 
   def create
     @place = Place.new(params["place"])
+    @place.user_id = @current_user.id
     @place.save
     redirect_to "/places"
   end
